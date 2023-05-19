58b45a525029bc090400b6af8400a118c97c352c
XerShade <jonathan.campbell.1992@hotmail.co.uk>
2023-05-19 14:33:01 -0400

    Create xsplugins-tealsanctuary.php

---
+++ xsplugins-tealsanctuary.php
@@ -0,0 +1,8 @@
+/*
+Plugin Name: XerShade's Plugns - Teal Sanctuary
+Plugin URI: https://github.com/XerShade/XSPlugins.TealSanctuary
+Description: A wordpress plugin that provides website specific code changes for 'tealsanctuary.com'.
+Author: XerShade
+Author URI: https://www.xershade.ca/
+Version: 1.0.0
+*/
