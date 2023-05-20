c3d1eb00c2bad9426fb20e5475b4fbc813cb45d8
XerShade <jonathan.campbell.1992@hotmail.co.uk>
2023-05-20 14:02:56 -0400

    Fixed missing php tag.

--- xsplugins-tealsanctuary.php
+++ xsplugins-tealsanctuary.php
@@ -1,5 +1,6 @@
+<?php
 /*
-Plugin Name: XerShade's Plugns - Teal Sanctuary
+Plugin Name: XerShade's Plugins - Teal Sanctuary
 Plugin URI: https://github.com/XerShade/XSPlugins.TealSanctuary
 Description: A wordpress plugin that provides website specific code changes for 'tealsanctuary.com'.
 Author: XerShade
