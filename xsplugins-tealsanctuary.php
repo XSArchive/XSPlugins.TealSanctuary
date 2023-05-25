14017453bbc4b123ce9ac643dbc37993d48bb0a9
XerShade <jonathan.campbell.1992@hotmail.co.uk>
2023-05-25 00:06:09 -0400

    Moved style overrides to plugin.

--- xsplugins-tealsanctuary.php
+++ xsplugins-tealsanctuary.php
@@ -2,8 +2,13 @@
 /*
 Plugin Name: XerShade's Plugins - Teal Sanctuary
 Plugin URI: https://github.com/XerShade/XSPlugins.TealSanctuary
-Description: A wordpress plugin that provides website specific code changes for 'tealsanctuary.com'.
+Description: A WordPress plugin that provides website specific code changes for 'tealsanctuary.com'.
 Author: XerShade
 Author URI: https://www.xershade.ca/
 Version: 1.0.0
 */
+
+add_action( 'wp_enqueue_scripts', 'ts_register_stylesheets', 100 );
+function ts_register_stylesheets() {
+	wp_enqueue_style( 'teal.sanctuary.overrides', plugins_url( 'css/teal.sanctuary.overrides.css', __FILE__ ) );
+}
